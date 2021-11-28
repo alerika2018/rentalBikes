e7784e58f7615997bee9d9ad70197480660c575d
Alejandra Hernandez <ehernandezvega00@mylangara.ca>
2021-11-28 13:43:53 -0800

    changes in validations

--- middleware/storeController.js
+++ middleware/storeController.js
@@ -24,7 +24,7 @@ const postStore = (req, res) => {
     })
     .catch((error) =>{
       console.log(error));
-      res.status(500).json({error})
+      // res.status(500).json({error})
     } 
 };
 
